5b85a1a0c8f8ac712aed119fde721c19f627c118
Talal <talalahmad257@gmail.com>
2022-11-20 08:26:10 +0000

    Add chatInput command handler

---
+++ src/handlers/interactions/ChatInput.ts
@@ -0,0 +1,12 @@
+import { ChatInputCommandInteraction, CommandInteraction } from "discord.js";
+import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
+
+export default async function handleChatInput(interaction: ChatInputCommandInteraction) {
+  const command = (await import(`../../commands/chatInput/${interaction.commandName}`)) as Command;
+  return command.execute(interaction);
+}
+
+export interface Command {
+  data: RESTPostAPIApplicationCommandsJSONBody;
+  execute(interaction: CommandInteraction): Promise<void>;
+}
