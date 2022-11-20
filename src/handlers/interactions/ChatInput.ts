1c296e9a3e688908ab3d39a2713ea43fd8b2b56f
Talal <talalahmad257@gmail.com>
2022-11-20 09:24:09 +0000

    Select default

--- src/handlers/interactions/ChatInput.ts
+++ src/handlers/interactions/ChatInput.ts
@@ -2,7 +2,8 @@ import { ChatInputCommandInteraction, CommandInteraction } from "discord.js";
 import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
 
 export default async function handleChatInput(interaction: ChatInputCommandInteraction) {
-  const command = (await import(`../../commands/chatInput/${interaction.commandName}`)) as Command;
+  const command = (await import(`../../commands/chatInput/${interaction.commandName}`))
+    .default as Command;
   return command.execute(interaction);
 }
 
