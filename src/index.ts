c50503d5aef03cf62cbad7c59260ff933c24d70b
Talal <talalahmad257@gmail.com>
2022-11-20 05:13:13 +0000

    Add ready event

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import { Client, GatewayIntentBits } from "discord.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds], allowedMentions: { parse: [] } });
 
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
 client.login(process.env.TOKEN);
