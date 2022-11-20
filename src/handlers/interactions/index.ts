75c995b9e635a074c9102fd0a5ede3e2b3138450
Talal <talalahmad257@gmail.com>
2022-11-20 08:26:26 +0000

    Handle ChatInputCommand interaction

--- src/handlers/interactions/index.ts
+++ src/handlers/interactions/index.ts
@@ -1,10 +1,12 @@
 import { Interaction } from "discord.js";
+import handleChatInput from "./ChatInput";
 
 
 export default async function handleInteraction(interaction: Interaction) {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === "ping") {
-    interaction.reply("Pong!");
+  try {
+    if (interaction.isChatInputCommand()) return handleChatInput(interaction);
+    
+  } catch (error) {
+    console.error(error);
   }
 }
