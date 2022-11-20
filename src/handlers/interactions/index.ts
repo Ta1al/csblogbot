8bce61a493fef938bbba917bea8d2cd31d4cf11f
Talal <talalahmad257@gmail.com>
2022-11-20 10:24:17 +0000

    Handle component interactions

--- src/handlers/interactions/index.ts
+++ src/handlers/interactions/index.ts
@@ -1,12 +1,13 @@
-import { Interaction, CommandInteraction } from "discord.js";
+import { Interaction, CommandInteraction, MessageComponentInteraction } from "discord.js";
 import { RESTPostAPIApplicationCommandsJSONBody } from "discord-api-types/v10";
-import handleChatInput from "./ChatInput";
+import handleChatInput from "./chatInput";
+import handleComponent from "./component";
 
 
 export default async function handleInteraction(interaction: Interaction) {
   try {
     if (interaction.isChatInputCommand()) return handleChatInput(interaction);
-    
+    if (interaction.isMessageComponent()) return handleComponent(interaction);
   } catch (error) {
     console.error(error);
   }
@@ -14,5 +15,12 @@ export default async function handleInteraction(interaction: Interaction) {
 
 export interface Command {
   data: RESTPostAPIApplicationCommandsJSONBody;
+  components?: Component[];
   execute(interaction: CommandInteraction): Promise<void>;
+}
+
+interface Component {
+  name: string;
+  hasPermission?(interaction: MessageComponentInteraction): boolean;
+  execute: (interaction: MessageComponentInteraction) => Promise<void>;
 }
