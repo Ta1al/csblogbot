850ea6578225bd11de597766f0ff3460616a1e6c
Talal <talalahmad257@gmail.com>
2022-11-25 06:24:34 +0000

    Implement blog command

--- src/commands/chatInput/blog.ts
+++ src/commands/chatInput/blog.ts
@@ -1,22 +1,79 @@
 import { Command } from "../../handlers/interactions";
-import { ApplicationCommandOptionType, ChatInputCommandInteraction } from "discord.js";
+import {
+  ApplicationCommandOptionType,
+  ChatInputCommandInteraction,
+  ComponentType,
+  EmbedBuilder,
+  MessageComponentInteraction
+} from "discord.js";
+import { getBlogPosts, Post } from "csblogscraper";
+import { makeCustomId } from "../../handlers/interactions/index";
 
-export default {
+const command: Command = {
   data: {
+    name: "blog",
     description: "Get the latest posts from CSGO Blog",
     options: [
       {
         type: ApplicationCommandOptionType.Number,
         name: "page",
-        description: "Page number",
+        description: "Page number"
       }
     ]
   },
 
+  components: [
+    {
+      name: "blog",
+      hasPermission: (interaction: MessageComponentInteraction) =>
+        interaction.user.id === interaction.message.interaction?.user.id,
+      execute: async (interaction: MessageComponentInteraction) => {
+        if (!interaction.isSelectMenu()) return;
+        const page = parseInt(interaction.customId.split(":")[3]),
+          posts = await getBlogPosts(page),
+          embeds = [makeEmbed(posts[parseInt(interaction.values[0])])],
+          components = [makeComponent(page, posts)];
+        return void (await interaction.update({ embeds, components }));
+      }
+    }
+  ],
+
   execute: async (interaction: ChatInputCommandInteraction) => {
-    return void (await interaction.reply({
-      content: "This command is not yet implemented",
-      ephemeral: true
-    }));
+    const page = interaction.options.getNumber("page") || 0,
+      posts = await getBlogPosts(page).catch(() => []);
+    if (!posts || !posts.length) return void interaction.reply("No posts found");
+
+    const embeds = [makeEmbed(posts[0])],
+      components = [makeComponent(page, posts)];
+    return void interaction.reply({ embeds, components });
   }
-} as Command;
+};
+
+export default command;
+
+function makeComponent(page: number, posts: Post[]) {
+  return {
+    type: ComponentType.ActionRow,
+    components: [
+      {
+        type: ComponentType.SelectMenu as const, // explain
+        customId: makeCustomId(command, `blog:${page}`),
+        options: posts.map((post, i) => ({
+          label: post.title,
+          description: post.date.toDateString(),
+          value: `${i}`
+        }))
+      }
+    ]
+  };
+}
+
+function makeEmbed(post: Post): EmbedBuilder {
+  return new EmbedBuilder()
+    .setTitle(post.title)
+    .setURL(post.link)
+    .setTimestamp(post.date)
+    .setDescription(post.content.slice(0, 4096))
+    .setImage(post.image)
+    .setColor("#2f3136");
+}
