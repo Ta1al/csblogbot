c5b8f7dbb8016a5e33ee1b339f49e30588105c4e
Talal <talalahmad257@gmail.com>
2022-11-29 08:39:47 +0000

    Add setup command

--- src/commands/chatInput/setup.ts
+++ src/commands/chatInput/setup.ts
@@ -1,4 +1,12 @@
-import { ApplicationCommandOptionType, ChatInputCommandInteraction } from "discord.js";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+import {
+  ApplicationCommandOptionType,
+  ChatInputCommandInteraction,
+  GuildMember,
+  PermissionFlagsBits,
+  TextBasedChannel
+} from "discord.js";
 import { Command } from "../../handlers/interactions";
 
 const command: Command = {
@@ -31,9 +39,32 @@ const command: Command = {
     ]
   },
 
-  execute: async (interaction: ChatInputCommandInteraction) => {
-    return void interaction.reply("TODO");
+  execute: async (interaction: ChatInputCommandInteraction<"cached">) => {
+    const type = interaction.options.getString("type", true) as "blog" | "update";
+    const channel = interaction.options.getChannel("channel", true) as TextBasedChannel;
+
+    if (isInavlid(channel, interaction.guild.members.me))
+      return void interaction.reply({
+        content:
+          "❌ Invalid channel\n\n" +
+          "> The channel is not a text channel or I'm missing permissions" +
+          "(`View Channel` or `Send Message`)",
+        ephemeral: true
+      });
+
+    return void interaction.reply({ content: `${type} ${channel}`, ephemeral: true });
   }
 };
 
 export default command;
+
+function isInavlid(channel: TextBasedChannel, member: GuildMember | null) {
+  if (!channel.isTextBased() || channel.isDMBased()) return true;
+  if (
+    !member
+      ?.permissionsIn(channel)
+      .has(PermissionFlagsBits.SendMessages | PermissionFlagsBits.ViewChannel)
+  )
+    return true;
+  return false;
+}
