712b162febfe5e60568ae408cd1ef868560cfe0d
Talal <talalahmad257@gmail.com>
2022-11-29 10:03:17 +0000

    Connect to DB

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { ActivityType, Client, GatewayIntentBits } from "discord.js";
 import handleInteraction from "./handlers/interactions";
+import { connect } from "mongoose";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -11,7 +12,11 @@ const client = new Client({
   }
 });
 
-client.once("ready", () => console.log("Ready!"));
+client.once("ready", client => {
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined");
+  connect(process.env.MONGO_URI).then(() => console.log("Connected to MongoDB"));
+  console.log("Ready as", client.user?.tag);
+});
 
 client.on("interactionCreate", handleInteraction);
 
