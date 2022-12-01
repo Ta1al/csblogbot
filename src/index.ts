306fb449f4da37fe9d9528aa15eea5f339c69b7a
Talal <talalahmad257@gmail.com>
2022-12-01 11:42:39 +0000

    Add handleNewPost()

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "dotenv/config";
 import { ActivityType, Client, GatewayIntentBits } from "discord.js";
 import handleInteraction from "./handlers/interactions";
 import { connect } from "mongoose";
+import handleNewPost from "./handlers/posts";
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -15,6 +16,7 @@ const client = new Client({
 client.once("ready", client => {
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI is not defined");
   connect(process.env.MONGO_URI).then(() => console.log("Connected to MongoDB"));
+  handleNewPost();
   console.log("Ready as", client.user?.tag);
 });
 
